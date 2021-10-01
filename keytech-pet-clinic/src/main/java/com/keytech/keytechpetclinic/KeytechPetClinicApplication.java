b6ca4d49ef89b0f504ceefef01a3226502ad33a6
benNjogu <kamandemachafi.gmail.com>
2021-10-01 10:50:49 +0300

    initial project add

---
+++ keytech-pet-clinic/src/main/java/com/keytech/keytechpetclinic/KeytechPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.keytech.keytechpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeytechPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeytechPetClinicApplication.class, args);
+	}
+
+}
---
+++ keytech-pet-clinic/src/test/java/com/keytech/keytechpetclinic/KeytechPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keytech.keytechpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeytechPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
