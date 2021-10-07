7b9e0ba5df6618bafc6602ad61a38679121fb1b2
benNjogu <kamandemachafi.gmail.com>
2021-10-07 16:14:16 +0300

    adding base entity. Closes #12

---
+++ keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/Model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.keytech.Model;
+
+public class BaseEntity {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+}
