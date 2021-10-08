b29d54602366234234a863c76a9ce6cf9ca370be
benNjogu <kamandemachafi.gmail.com>
2021-10-08 16:59:44 +0300

    Adding vets page and controller. Closes #7

--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Controllers/VetController.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Controllers/VetController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class VetController {
 
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String index() {
+	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+	public String listVets() {
 		
-		return "index";
+		return "Vets/index";
 	}
 	
 }
--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/keytechpetclinic/KeytechPetClinicApplication.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/keytechpetclinic/KeytechPetClinicApplication.java
@@ -2,8 +2,10 @@ package com.keytech.keytechpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.keytech.Controllers")
 public class KeytechPetClinicApplication {
 
 	public static void main(String[] args) {
