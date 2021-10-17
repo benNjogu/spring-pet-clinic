c092331b22a5d3af62f3afbcc72a36a94173aa9b
benNjogu <kamandemachafi.gmail.com>
2021-10-17 17:22:50 +0300

    Added Vets List and updated Controller

--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Controllers/VetController.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Controllers/VetController.java
@@ -1,14 +1,28 @@
 package com.keytech.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.keytech.Services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
 
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
 		
 		return "Vets/index";
 	}
