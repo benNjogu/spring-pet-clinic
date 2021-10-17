938465d0d4a657d37d7aa6f4e7eb9fa5c8e5f28d
benNjogu <kamandemachafi.gmail.com>
2021-10-17 15:48:49 +0300

    Adding spring Config. Closes #32

--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/keytechpetclinic/KeytechPetClinicApplication.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/keytechpetclinic/KeytechPetClinicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.keytech.Controllers", "com.keytech.Bootstrap"})
+@ComponentScan(basePackages = {"com.keytech.Controllers", "com.keytech.Bootstrap", "com.keytech.Services.Map"})
 public class KeytechPetClinicApplication {
 
 	public static void main(String[] args) {
