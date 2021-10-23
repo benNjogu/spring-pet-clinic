5f9a7720116fbc821c2b766044e3a0c381863975
benNjogu <kamandemachafi.gmail.com>
2021-10-23 10:26:23 +0300

    Added properties to owners.closes #45

--- keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/Model/Owner.java
+++ keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/Model/Owner.java
@@ -2,9 +2,36 @@ package com.keytech.Model;
 
 import java.util.Set;
 
-public class Owner extends Person{
+public class Owner extends Person {
 
+	private String adderess;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
+
+	public String getAdderess() {
+		return adderess;
+	}
+
+	public void setAdderess(String adderess) {
+		this.adderess = adderess;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
@@ -13,7 +40,5 @@ public class Owner extends Person{
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
-	
-	
-	
+
 }
