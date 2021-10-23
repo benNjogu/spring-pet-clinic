9bee8a277e1129a887669188571e6e4cd5483816
benNjogu <kamandemachafi.gmail.com>
2021-10-23 10:17:11 +0300

    Added specialities to vets.closes #44

---
+++ keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/Model/Speciality.java
@@ -0,0 +1,17 @@
+package com.keytech.Model;
+
+public class Speciality extends BaseEntity{
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
