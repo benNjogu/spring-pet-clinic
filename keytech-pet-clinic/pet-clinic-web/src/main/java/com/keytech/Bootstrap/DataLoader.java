7ad618eaf96c0a5f5a8e7d9c4e9a8623c876b3de
benNjogu <kamandemachafi.gmail.com>
2021-10-23 11:57:14 +0300

    Bootstrapping pet types.closes #48

--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Bootstrap/DataLoader.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/Bootstrap/DataLoader.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.keytech.Services.OwnerService;
+import com.keytech.Services.PetTypeService;
 import com.keytech.Model.Owner;
+import com.keytech.Model.PetType;
 import com.keytech.Model.Vet;
 import com.keytech.Services.VetService;
 
@@ -13,16 +15,28 @@ public class DataLoader implements CommandLineRunner{
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 	
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		super();
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+	
+		PetType savedCatPetType = petTypeService.save(cat);
 		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Alice");
