795c8ba7b224dd615d332c28f8a70eaeac0c7905
benNjogu <kamandemachafi.gmail.com>
2022-02-17 18:41:46 +0300

    Adding builder pattern to petType.

--- keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/model/PetType.java
+++ keytech-pet-clinic/pet-clinic-data/src/main/java/com/keytech/model/PetType.java
@@ -14,11 +14,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
+	@Builder
+	public PetType(Long id, String name) {
+		super(id);
+		this.name = name;
+	}
+	
 	@Column(name = "name")
 	private String name;
 	
@@ -26,4 +31,5 @@ public class PetType extends BaseEntity{
 	public String toString() {
 		return name;
 	}
+
 }
