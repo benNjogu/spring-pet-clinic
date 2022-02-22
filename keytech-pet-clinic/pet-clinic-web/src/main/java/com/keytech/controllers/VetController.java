35c89b7180db0b0d9dbd5c5dcda0bbddab1f45a3
benNjogu <kamandemachafi.gmail.com>
2022-02-22 21:54:29 +0300

    Added JSON endpoint for vets, closes #105

--- keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/controllers/VetController.java
+++ keytech-pet-clinic/pet-clinic-web/src/main/java/com/keytech/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.keytech.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.keytech.model.Vet;
 import com.keytech.services.VetService;
 
 @Controller
@@ -26,5 +31,16 @@ public class VetController {
 		return "vets/index";
 	}
 	
+	@GetMapping("api/vets")
+	public @ResponseBody Set<Vet> getVetsJSON() {
+		
+		return vetService.findAll();
+	}
+	
+	/**
+	 * @The above produces the below:
+	 * [{"id":1,"firstName":"Stephen","lastName":"Gathaiya","specialities":[{"id":1,"description":"Radiology","new":false}],"new":false},
+	 * {"id":2,"firstName":"LE","lastName":"Kimanga","specialities":[{"id":3,"description":"Dentistry","new":false}],"new":false}]
+	 */
 	
 }
